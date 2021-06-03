42111983fdc7552d91ae17cc30c9bb21f6b21ede
trayan89 <trayan.gospodinov@gmail.com>
2021-06-03 10:02:37 +0300

    commmit

---
+++ src/main/java/com/gsmarena/gsmarena/GsmarenaApplication.java
@@ -0,0 +1,13 @@
+package com.gsmarena.gsmarena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsmarenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsmarenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsmarena/gsmarena/GsmarenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsmarena.gsmarena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsmarenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
